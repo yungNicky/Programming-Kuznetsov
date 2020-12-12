60385068d654ea19e1681a76f61a9ebe2ecdb63d
yungNicky <72757712+yungNicky@users.noreply.github.com>
2020-12-12 16:24:51 +0300

    Центрувати заданий рядок

--- lab06/task1/src/main.c
+++ lab06/task1/src/main.c
@@ -1,34 +1,33 @@
-
 #define LEN 15 // Указываем кол-во символов
 int main()
 {
-	char filler = "_"; // Указываем заполнитель
+	char filler = ('_'); // Указываем заполнитель
 	char strIn[] = "Ivanov"; // Вводим необходимый текст
 	char strResult[LEN];
 	int lenIn = 0; // Кол-во элементов исходного массива
 
-	for (int i = 0; ; i++) { // Высчитываем кол-во заполнителей
+	for (int i = 0; ; i++) { // Высчитываем кол-во символов
 		if (strIn[i] == '\0') { // Если элемент массива == '\o' - прекращается действие
 			break;
-		} else if (strIn[i] != 0) {
+		}
+		else if (strIn[i] != 0) {
 			lenIn++;
 		}
 	}
-	
-	int lenFiller = LEN - lenIn - 1; // Высчитываем сколько будет символов заполнителя
-	int halfLenFiller = lenFiller / 2; // Кол-во символов заполнителя перед и после текста
 
-	for (int i = 0; i < halfLenFiller; i++) {
+	int lenFiller = LEN - lenIn; // Высчитываем сколько будет символов заполнителя
+	int halfLenFiller = lenFiller / 2; // Кол-во символов заполнителя перед текстом
+
+	for (int i = 0; i <= LEN; i++) { // Заполняем весь массив заполнителем
 		strResult[i] = filler;
 	}
 	
-	
-	for (int i = halfLenFiller; i < (halfLenFiller = lenIn); ) {
+	for (int i = halfLenFiller; i < (halfLenFiller + lenIn); ) { // Записываем текст в центр
 		for (int j = 0; j < lenIn; j++) {
 			strResult[i] = strIn[j];
 			i++;
 		}
 	}
 
-return 0;
+	return 0;
 }
